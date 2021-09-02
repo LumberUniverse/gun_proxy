eeb5ae81f4be9184ae56d8bf53cd0cfbcac90616
LumberUniverseYT <33379217+LumberUniverseYT@users.noreply.github.com>
2021-09-02 16:37:24 +0200

    changes

--- src/client/units/Viewmodel.ts
+++ src/client/units/Viewmodel.ts
@@ -1,96 +1,94 @@
 import { Players, ReplicatedStorage, RunService, Workspace } from "@rbxts/services";
 
 export function CreateArm(name: string) {
-    var Camera = Workspace.CurrentCamera
-    var Arm = new Instance("Model")
-    Arm.Name = name
-    Arm.Parent = Camera
+	const Camera = Workspace.CurrentCamera;
 
-    var ActionArm = new Instance("Part")
-	ActionArm.Parent = Arm
-	ActionArm.Name = name
-	ActionArm.Anchored = true
-	ActionArm.CanCollide = false
-	ActionArm.CanTouch = false
-	ActionArm.Material = Enum.Material.SmoothPlastic
-	ActionArm.Color = Color3.fromRGB(204, 142, 105)
-	ActionArm.Size = new Vector3(1.85, 0.75, 0.75)
+	const ActionArm = new Instance("Part");
+	ActionArm.Parent = Camera;
+	ActionArm.Name = name;
+	ActionArm.Anchored = true;
+	ActionArm.CanCollide = false;
+	ActionArm.CanTouch = false;
+	ActionArm.Material = Enum.Material.SmoothPlastic;
+	ActionArm.Color = Color3.fromRGB(204, 142, 105);
+	ActionArm.Size = new Vector3(1.85, 0.75, 0.75);
 
-    var Elbow = new Instance("Part")
-    Elbow.Parent = Arm
-    Elbow.Name = name
-    Elbow.Anchored = false
-    Elbow.CanCollide = false
-    Elbow.CanTouch = false
-    Elbow.Material = Enum.Material.SmoothPlastic
-    Elbow.Color = Color3.fromRGB(204, 142, 105)
-    Elbow.Size = new Vector3(2.25, 0.75, 0.75)
-    
-    var Joint = new Instance("Motor6D")
-    Joint.Name = "Joint"
-    Joint.Parent = ActionArm
-    Joint.Part0 = ActionArm
-    Joint.Part1 = Elbow
-    Joint.C0 = new CFrame(ActionArm.Size.X/2 - 0.075, 0, 0)
-        ?.mul(CFrame.Angles(0, 0, math.rad(22.5)))
-        ?.mul(new CFrame(Elbow.Size.X/2-0.075, 0, 0))
-
-    Arm.PrimaryPart = ActionArm
-
-    return Arm
+	return ActionArm;
 }
 
 export function Viewmodel(gun: any) {
-    var Gun: any = ReplicatedStorage.FindFirstChild("TestViewmodel")?.Clone()
-    var Camera = Workspace.CurrentCamera
-    var LeftArm = CreateArm("LeftArm")
-    var RightArm = CreateArm("RightArm")
-    var LocalPlayer = Players.LocalPlayer
-    var LastShot: any = null
-    var RecoilCollected = 0
+	const Gun: any = ReplicatedStorage.FindFirstChild("TestViewmodel")?.Clone();
+	const Camera = Workspace.CurrentCamera;
+	const LeftArm = CreateArm("LeftArm");
+	const RightArm = CreateArm("RightArm");
+	const LocalPlayer = Players.LocalPlayer;
+	const LastShot: any = undefined;
+	let RecoilCollected = 0;
 
-    // MOVE TO GUN
-    var RecoilSpeed = 9
-    var RecoilHeight = 0.7
-    var Offset = new CFrame(0.5, -1.5, -2)
-    
-    RunService.RenderStepped.Connect(() => {
-        let Character = LocalPlayer.Character
-        let Humanoid = Character?.FindFirstChildOfClass("Humanoid")
+	// MOVE TO GUN
+	const RecoilSpeed = 9;
+	const RecoilHeight = 0.7;
+	const Offset = new CFrame(0.5, -1.5, -2);
 
-        if (!Gun) return
+	RunService.RenderStepped.Connect(() => {
+		const Character = LocalPlayer.Character;
+		const Humanoid = Character?.FindFirstChildOfClass("Humanoid");
 
-        if (Humanoid && Camera && Humanoid.RootPart && Humanoid.MoveDirection.Magnitude <= 0) {
-            let now = os.time()
-            let amount = - math.cos(now) / 5
-            let recoild_amount = (LastShot && math.clamp(-RecoilSpeed * (os.clock()-LastShot) + RecoilHeight, -RecoilCollected/RecoilSpeed, RecoilHeight)) || 0
-            let Goal = Camera.CFrame.mul(CFrame.Angles(math.rad((((recoild_amount <= 0 && RecoilCollected <= 0) && amount) || 0) + recoild_amount), 0, 0))
+		if (!Gun) return;
 
-            Camera.CFrame = Camera.CFrame.Lerp(Goal, 0.5)
-            RecoilCollected += recoild_amount
-            
-            if (recoild_amount <= 0 && RecoilCollected <= 0) {
-                let X = math.cos(now * 9) / 5
-                let Y = math.abs(math.sin(now * 12)) / 5
-                let bobble = new Vector3(X, Y, 0).mul(math.min(1, Humanoid.RootPart.Velocity.Magnitude / Humanoid.WalkSpeed))
-                Humanoid.CameraOffset = Humanoid.CameraOffset.Lerp(bobble, 0.25)
-            }
-        } else if (Humanoid && Camera && Humanoid.RootPart) {
-            let recoild_amount = (LastShot && math.clamp(-RecoilSpeed * (os.clock()-LastShot) + RecoilHeight, -RecoilCollected/RecoilSpeed, RecoilHeight)) || 0
-            Camera.CFrame = Camera.CFrame.Lerp(Camera.CFrame.mul(CFrame.Angles(math.rad(recoild_amount), 0, 0)), 0.5)
-            RecoilCollected += recoild_amount
-        }
-        
-        if (Gun && Gun.PrimaryPart && Camera && LeftArm && LeftArm.PrimaryPart && RightArm && RightArm.PrimaryPart) {
-            Gun.SetPrimaryPartCFrame(Camera.CFrame.mul(Offset))
+		if (Humanoid && Camera && Humanoid.RootPart && Humanoid.MoveDirection.Magnitude <= 0) {
+			const now = os.time();
+			const amount = -math.cos(now) / 5;
+			const recoild_amount =
+				(LastShot &&
+					math.clamp(
+						-RecoilSpeed * (os.clock() - LastShot) + RecoilHeight,
+						-RecoilCollected / RecoilSpeed,
+						RecoilHeight,
+					)) ||
+				0;
+			const Goal = Camera.CFrame.mul(
+				CFrame.Angles(
+					math.rad(((recoild_amount <= 0 && RecoilCollected <= 0 && amount) || 0) + recoild_amount),
+					0,
+					0,
+				),
+			);
 
-            LeftArm.PrimaryPart.CFrame = (Gun.PrimaryPart.LeftArmAttach.WorldCFrame as CFrame)
-                ?.mul(CFrame.Angles(0, math.rad(180), 0))
-                ?.mul(new CFrame(LeftArm.PrimaryPart.Size.X, 0, 0))
+			Camera.CFrame = Camera.CFrame.Lerp(Goal, 0.5);
+			RecoilCollected += recoild_amount;
 
-            RightArm.PrimaryPart.CFrame = (Gun.PrimaryPart.RightArmAttach.WorldCFrame as CFrame)
-                ?.mul(CFrame.Angles(0, math.rad(180), 0))
-                ?.mul(new CFrame(RightArm.PrimaryPart.Size.X, 0, 0))
-        }
-    })
+			if (recoild_amount <= 0 && RecoilCollected <= 0) {
+				const X = math.cos(now * 9) / 5;
+				const Y = math.abs(math.sin(now * 12)) / 5;
+				const bobble = new Vector3(X, Y, 0).mul(
+					math.min(1, Humanoid.RootPart.Velocity.Magnitude / Humanoid.WalkSpeed),
+				);
+				Humanoid.CameraOffset = Humanoid.CameraOffset.Lerp(bobble, 0.25);
+			}
+		} else if (Humanoid && Camera && Humanoid.RootPart) {
+			const recoild_amount =
+				(LastShot &&
+					math.clamp(
+						-RecoilSpeed * (os.clock() - LastShot) + RecoilHeight,
+						-RecoilCollected / RecoilSpeed,
+						RecoilHeight,
+					)) ||
+				0;
+			Camera.CFrame = Camera.CFrame.Lerp(Camera.CFrame.mul(CFrame.Angles(math.rad(recoild_amount), 0, 0)), 0.5);
+			RecoilCollected += recoild_amount;
+		}
+
+		if (Gun && Gun.PrimaryPart && Camera && LeftArm && RightArm) {
+			Gun.SetPrimaryPartCFrame(Camera.CFrame.mul(Offset));
+
+			LeftArm.CFrame = (Gun.PrimaryPart.LeftArmAttach.WorldCFrame as CFrame)
+				?.mul(CFrame.Angles(0, math.rad(180), 0))
+				?.mul(new CFrame(LeftArm.Size.X, 0, 0));
+
+			RightArm.CFrame = (Gun.PrimaryPart.RightArmAttach.WorldCFrame as CFrame)
+				?.mul(CFrame.Angles(0, math.rad(180), 0))
+				?.mul(new CFrame(RightArm.Size.X, 0, 0));
+		}
+	});
 }
