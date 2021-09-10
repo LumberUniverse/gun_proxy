4cc2cbc8b5b7ae5215555bc833725b5974a23b2d
Marcus <Ukendio@gmail.com>
2021-09-10 21:01:34 +0200

    write it more idiomatic

--- src/client/units/Cam.ts
+++ src/client/units/Cam.ts
@@ -1,19 +1,10 @@
 import { ThisFabricUnit, UnitDefinition } from "@rbxts/fabric";
 import { Option } from "@rbxts/rust-classes";
-import { Players, RunService, TweenService, Workspace } from "@rbxts/services";
+import { Players, Workspace } from "@rbxts/services";
 import { GunSide } from "shared/Types";
-
-const apply_bobbing = (add: number, speed: number, modifier: number) => math.sin(os.clock() * add * speed) * modifier;
-const lerp_number = (a: number, b: number, t: number) => a + (b - a) * t;
 
 interface Cam extends UnitDefinition<"Cam"> {
 	ref: Camera;
-	localPlayer: Player;
-	character: Model | undefined;
-	humanoid: Humanoid | undefined;
-
-	last_shot: Option<number>;
-	recoil_collected: number;
 
 	defaults: {
 		recoil_speed: number;
@@ -24,6 +15,9 @@ interface Cam extends UnitDefinition<"Cam"> {
 		view_model?: Instance;
 		camera_type: Enum.CameraType;
 		right_handed: boolean;
+
+		last_shot: Option<number>;
+		recoil_collected: number;
 	};
 
 	offset?: CFrame;
@@ -46,15 +40,13 @@ declare global {
 	}
 }
 
+const player = Players.LocalPlayer;
+const character = player.Character ?? player.CharacterAdded.Wait()[0];
+const humanoid = character?.FindFirstChildOfClass("Humanoid");
+
 export = identity<Cam>({
 	name: "Cam",
 	ref: Workspace.CurrentCamera!,
-	localPlayer: Players.LocalPlayer,
-	character: Players.LocalPlayer.Character,
-	humanoid: Players.LocalPlayer.Character?.FindFirstChildOfClass("Humanoid"),
-	
-	last_shot: Option.none<number>(),
-	recoil_collected: 0,
 
 	defaults: {
 		recoil_speed: 9,
@@ -64,26 +56,15 @@ export = identity<Cam>({
 		current_camera_cframe: new CFrame(),
 		camera_type: Enum.CameraType.Custom,
 		right_handed: true,
+
+		last_shot: Option.none<number>(),
+		recoil_collected: 0,
 	},
 
 	onInitialize: function (this) {
 		Workspace.GetPropertyChangedSignal("CurrentCamera").Connect(() => {
 			this.ref = Workspace.CurrentCamera!;
 		});
-
-		this.offset = this.defaults.offset,
-
-		this.localPlayer.CharacterAdded.Connect((character) => {
-			this.character = character;
-			this.humanoid = character?.FindFirstChildOfClass("Humanoid");
-		})
-		
-		this.localPlayer = Players.LocalPlayer;
-		this.character = this.localPlayer.Character;
-		this.humanoid = this.character?.FindFirstChildOfClass("Humanoid");
-
-		this.last_shot = Option.none<number>();
-		this.recoil_collected = 0;
 	},
 
 	change_view_model: function (this, view_model) {
@@ -92,9 +73,9 @@ export = identity<Cam>({
 
 	aim_down_sight: function (this, s) {
 		for (let i = 0; i < 60; i++) {
-			this.offset = this.offset?.Lerp(s.CFrame, i/60);
+			this.offset = this.offset?.Lerp(s.CFrame, i / 60);
 		}
-		
+
 		this.addLayer("Camera", { current_camera_cframe: s.CFrame, camera_type: Enum.CameraType.Scriptable });
 	},
 
@@ -106,16 +87,16 @@ export = identity<Cam>({
 		this.addLayer("gun_side", { right_handed: side === GunSide.Right });
 	},
 
-	adjust_camera: function(this) {
+	adjust_camera: function (this) {
 		const gun = this.get("view_model");
-		const Humanoid = this.humanoid;
-		const recoil_speed = this.defaults.recoil_speed;
-		const recoil_height = this.defaults.recoil_height;
-		const offset = this.offset;
+		const recoil_speed = this.get("recoil_speed");
+		const recoil_height = this.get("recoil_height");
+		const recoil_collected = this.get("recoil_collected");
+		const last_shot = this.get("last_shot");
 
 		if (!gun) return;
 
-		if (Humanoid && this.ref && Humanoid.RootPart && Humanoid.MoveDirection.Magnitude <= 0) {
+		if (humanoid && this.ref && humanoid.RootPart && humanoid.MoveDirection.Magnitude <= 0) {
 			const now = os.time();
 			const amount = -(math.cos(now) / 5);
 
@@ -123,63 +104,66 @@ export = identity<Cam>({
 				math.clamp(
 					-recoil_speed *
 						(os.clock() -
-							this.last_shot.match(
+							last_shot.match(
 								(n) => n,
 								() => os.clock(),
 							)) +
 						recoil_height,
-					-this.recoil_collected / recoil_speed,
+					-recoil_collected / recoil_speed,
 					recoil_height,
 				) ?? 0;
 
 			const Goal = this.ref.CFrame.mul(
 				CFrame.Angles(
-					math.rad((recoil_amount <= 0 && this.recoil_collected <= 0 ? amount : 0) + recoil_amount),
+					math.rad((recoil_amount <= 0 && recoil_collected <= 0 ? amount : 0) + recoil_amount),
 					0,
 					0,
 				),
 			);
 
 			this.ref.CFrame = this.ref.CFrame.Lerp(Goal, 0.5);
-			this.recoil_collected += recoil_amount;
+			this.addLayer("recoil", { recoil_collected: this.get("recoil_collected") + recoil_amount });
 
-			if (recoil_amount <= 0 && this.recoil_collected <= 0) {
+			if (recoil_amount <= 0 && recoil_collected <= 0) {
 				const X = math.cos(now * 9) / 5;
 				const Y = math.abs(math.sin(now * 12)) / 5;
 				const bobble = new Vector3(X, Y, 0).mul(
-					math.min(1, Humanoid.RootPart.Velocity.Magnitude / Humanoid.WalkSpeed),
+					math.min(1, humanoid.RootPart.Velocity.Magnitude / humanoid.WalkSpeed),
 				);
-				Humanoid.CameraOffset = Humanoid.CameraOffset.Lerp(bobble, 0.25);
+				humanoid.CameraOffset = humanoid.CameraOffset.Lerp(bobble, 0.25);
 			}
-		} else if (Humanoid && this.ref && Humanoid.RootPart) {
-			const recoil_amount = this.last_shot
+		} else if (humanoid && this.ref && humanoid.RootPart) {
+			const recoil_amount = last_shot.isSome()
 				? math.clamp(
-						-recoil_speed * (os.clock() - this.last_shot.expect("no last shot")) + recoil_height,
-						-this.recoil_collected / recoil_speed,
+						-recoil_speed * (os.clock() - last_shot.expect("no last shot")) + recoil_height,
+						-recoil_collected / recoil_speed,
 						recoil_height,
-				)
+				  )
 				: 0;
-			this.ref.CFrame = this.ref.CFrame.Lerp(this.ref.CFrame.mul(CFrame.Angles(math.rad(recoil_amount), 0, 0)), 0.5);
-			this.recoil_collected += recoil_amount;
+			this.ref.CFrame = this.ref.CFrame.Lerp(
+				this.ref.CFrame.mul(CFrame.Angles(math.rad(recoil_amount), 0, 0)),
+				0.5,
+			);
+			this.addLayer("recoil", { recoil_collected: this.get("recoil_collected") + recoil_amount });
 		}
 
 		if (this.ref && gun.FindFirstChild("Handle")) {
 			const handle = gun.FindFirstChild("Handle") as Part;
 
-			if (!handle) return
+			if (!handle) return;
 
 			handle.CFrame = this.ref.CFrame.mul(this.offset || this.defaults.offset);
 
-			const left_arm = gun.FindFirstChild('LeftArm') as Part;
-			const right_arm = gun.FindFirstChild('RightArm') as Part;
+			const left_arm = gun.FindFirstChild("LeftArm") as Part;
+			const right_arm = gun.FindFirstChild("RightArm") as Part;
 
-			left_arm.CFrame = (handle?.FindFirstChild("LeftArmAttach") as Attachment).WorldCFrame?.mul(CFrame.Angles(0, math.rad(180), 0))?.mul(
-				new CFrame(left_arm.Size.X, 0, 0),
-			);
+			left_arm.CFrame = (handle?.FindFirstChild("LeftArmAttach") as Attachment).WorldCFrame?.mul(
+				CFrame.Angles(0, math.rad(180), 0),
+			)?.mul(new CFrame(left_arm.Size.X, 0, 0));
 
-			right_arm.CFrame = (handle?.FindFirstChild("RightArmAttach") as Attachment).WorldCFrame?.mul(CFrame.Angles(0, math.rad(180), 0))?.mul(
-				new CFrame(right_arm.Size.X, 0, 0),
-			);
+			right_arm.CFrame = (handle?.FindFirstChild("RightArmAttach") as Attachment).WorldCFrame?.mul(
+				CFrame.Angles(0, math.rad(180), 0),
+			)?.mul(new CFrame(right_arm.Size.X, 0, 0));
 		}
 	},
 
